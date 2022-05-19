8ac25876285871052339e6aa10aa333eed36fb6b
ferranjrr <ferrandelavargaantoja@gmail.com>
2022-05-19 17:53:07 +0200

    Crear .cpp i .h

---
+++ VCode/Passaport8Object.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "simulationobject.h"
+#include "simulator.h"
+
+class Passaport8Object : public CSimulationObject
+    public: 
+        CSimulationObject
+
+    protected:
+
+{
+};
+
